9fc45d02a0c16561587fb9add0f87849f6346882
Bronson Brown-deVost <bronsonbdevost@aim.com>
2020-04-15 12:08:36 +0200

    documentation updates

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # rust-geo-normalized
 
-Creates a new version of the rust geo Polygon/Multipolygon types that follow the OGC winding rules. 
+Creates a new instance of the rust geo/geo-types Polygon/Multipolygon that follows the OGC winding rules. 
+
+The rust geo and geo-types crates are not as strict as the OGC guidelines, and allow for polygons with inner and outer rings in any winding order. This trait returns a Polygon/Multipolygon where all outer rings are clockwise, and all inner rings are anti-clockwise.
+
+## Examples
+
+```rust
+// Anti-clockwise winding order for outer ring
+let bad = polygon![
+        (x: 1.0, y: 1.0),
+        (x: 4.0, y: 1.0),
+        (x: 4.0, y: 4.0),
+        (x: 1.0, y: 4.0),
+        (x: 1.0, y: 1.0),
+        ];
+
+// Clockwise winding order for outer ring
+let good = polygon![
+        (x: 1.0, y: 1.0),
+        (x: 1.0, y: 4.0),
+        (x: 4.0, y: 4.0),
+        (x: 4.0, y: 1.0),
+        (x: 1.0, y: 1.0),
+        ];
+
+let norm = bad.normalized();
+// norm should have the same points and shape as `bad` but in the valid winding order
+assert_eq!(norm, good);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,35 @@ use num_traits;
 /// This trait returns a new geo-types Polygon/Multipolygon that follows the OGC winding rules
 ///
 /// The rust geo and geo-types crates are not as strict as the OGC guidelines,
-/// and allow for polygons with inner and outer rings in any direction.  This
-/// trait returns a Polygon/Multipolygon where all outer rings are clockwise,
+/// and allow for polygons with inner and outer rings in any winding order.
+/// This trait returns a Polygon/Multipolygon where all outer rings are clockwise,
 /// and all inner rings are anti-clockwise.
+///
+/// # Examples
+///
+/// ```
+/// // Anti-clockwise winding order for outer ring
+/// let bad = polygon![
+///         (x: 1.0, y: 1.0),
+///         (x: 4.0, y: 1.0),
+///         (x: 4.0, y: 4.0),
+///         (x: 1.0, y: 4.0),
+///         (x: 1.0, y: 1.0),
+///         ];
+///
+/// // Clockwise winding order for outer ring
+/// let good = polygon![
+///         (x: 1.0, y: 1.0),
+///         (x: 1.0, y: 4.0),
+///         (x: 4.0, y: 4.0),
+///         (x: 4.0, y: 1.0),
+///         (x: 1.0, y: 1.0),
+///         ];
+///
+/// let norm = bad.normalized();
+/// // norm should have the same points and shape as `bad` but in the valid winding order
+/// assert_eq!(norm, good);
+/// ```
 ///
 pub trait Normalized<T: num_traits::Float> {
     fn normalized(&self) -> Self;
